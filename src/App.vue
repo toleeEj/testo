202ab83b23280692a4114b0498eba8fcf7794299
Tolesa Ijara <tolesa.ijara@addisbanksc.local>
2024-11-21 06:09:39 -0500

    this is just to test new window

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>{{message}}</h1>
+  </div>
+</template>
+
+<script>
+  export default{
+    data(){
+      return{
+        message: "this is just to try"
+      }
+    }
+  }
+</script>
+<style scoped>
+
+</style>
